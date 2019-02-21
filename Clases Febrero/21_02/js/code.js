eece51e57c4dce726ac8a597d68a8165bc74f77b
Jasorza <41799764+Jasorza@users.noreply.github.com>
2019-02-21 09:44:04 -0500

    3

--- Clases Febrero/21_02/js/code.js
+++ Clases Febrero/21_02/js/code.js
@@ -13,10 +13,45 @@ var res = tex.replace('Imagenes', 'videos');
 document.write('reemplazando palabras ' + res + '<br>');
 //** --------------------------------------------------------------- */
 
+//** 1ra Forma para hacer un Array */
 var nombres = new Array('Roxana', 'Gomez', 'Carlos', 'Miguel');
 document.write(nombres[0]);
 
+//** 2da Forma para hacer un Array */
 var nombre2 = new Array();
 nombre2[0] = 'Dilan';
 nombre2[1] = 'Santiago';
 nombre2[2] = 'Sebastian';
+nombre2[3] = 'Lorena';
+
+//** 3ra Forma para hacer un Array */
+var nombre3 = ['Jhon', 'Carlor', 'Brayan', 'Camilo'];
+//** --------------------------------------------------------------- */
+
+//** Metodos de Arreglos */
+var codes = ['Js', 'Ruby', 'Java', 'PHP', 'C#'];
+var movies = ['Watchmen', 'Avengers', 'Deadpool'];
+
+//** Agrando */
+codes.push('Swift');
+
+function printArray(arreglo, opcion) {
+    document.write('<h1>Imprimiendo Arreglo de ' + opcion + '</h1>');
+    document.write('<ul>');
+    for (const key in arreglo) {
+        document.write('<li>'+arreglo[key]+'</li>');
+    }
+    document.write('</ul>');
+}
+printArray(codes,'Lenguajes de Programación');
+printArray(movies,'Peliculas de Super Heroes');
+//** --------------------------------------------------------------- */
+
+//** Borrando el ultimo */
+movies.pop();
+printArray(movies,'Peliculas de Super Heroes');
+//** --------------------------------------------------------------- */
+
+//** Borrando el ultimo */
+var indice = prompt('Ingrese el Indice');
+var Array = prompt('Ingrese L para Programación o P para Peliculas');
