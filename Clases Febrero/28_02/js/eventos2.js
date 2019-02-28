e02c22502c8d18a25a54f9e5f17664087e91d498
Jasorza <41799764+Jasorza@users.noreply.github.com>
2019-02-28 13:33:59 -0500

    fin

--- Clases Febrero/28_02/js/eventos2.js
+++ Clases Febrero/28_02/js/eventos2.js
@@ -1,14 +1,67 @@
+let caja = document.querySelector('.cja');
+let btnAdd = document.querySelector('#btnAdd');
+let btnAddano = document.querySelector('#btnAddano');
+let btnAnimar2 = document.querySelector('btnAnimar2');
+let btnAddOnMouseOver = document.querySelector('#btnAddOnMouseOver');
+
+//** -------------------------------------------------------------------------- */
 function cambiarColor() {
     let btnColor = document.querySelector('#btnColor');
-    let color=btnColor.style.background;
+    let color = btnColor.style.background;
 
-    
-    console.log(color)
+    console.log(color);
 
-
+    btnColor.style.padding = '30px';
+    btnColor.style.border = '10px solid blue';
     if (color == 'red') {
         btnColor.style.background = 'black';
     } else {
-        color.style.background = 'red'
+        btnColor.style.background = 'red';
     }
-}
+};
+
+//** ---------------------------------------------------- */
+/** AddEventListener */
+btnAdd.addEventListener('click', cambiarCaja);
+
+function cambiarCaja() {
+    caja.style.width = '300px';
+    caja.style.height = '500px';
+    caja.style.background = '#5c5c5c';
+};
+
+btnAddano.addEventListener('click', function () {
+    caja.style.width = '500px';
+    caja.style.height = '300px';
+    caja.style.background = '#566565';
+});
+
+btnaddOnClick.onclick = cambiarCaja2;
+
+function cambiarCaja2() {
+    caja.style.width = '700px';
+    caja.style.height = '800px';
+    caja.style.background = '#fa8556';
+};
+
+//** ---------------------------------------------------- */
+btnAddOnMouseOver.onmouseover = function () {
+    btnaddmouseover.style.background = 'oragen';
+    btnaddmouseover.style.transsition = 'all linear 5s';
+
+};
+
+//** ---------------------------------------------------- */
+btnAnimar.onmouseover = function () {
+    this.classList.add('.Animar');
+};
+
+btnAnimar.onmouseout = function () {
+    this.classList.remove('.Animar');
+};
+
+//** ---------------------------------------------------- */
+
+btnAnimar.onmouseover = function () {
+    this.classList.toggle('.Animar2');
+};
---
+++ Clases Febrero/28_02/js/funciones.js
@@ -0,0 +1,8 @@
+let bombillo = document.querySelector('#bombillo');
+
+bombillo.onclick = function () {
+    bombillo.src = 'img/img1.jpg';
+};
+bombillo.ondblclick = function () {
+    bombillo.src = 'img/img2.jpg';
+}
